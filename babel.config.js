2a075efc5f45b201913978cb085255dfa5bfbc3a
Priscila Oliveira <poliveira13@hotmail..com>
2023-05-29 13:36:07 -0300

    feat(babel): add babel and lint-staged

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,36 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo']
-  };
-};
+module.exports = function (api) {
+    api.cache(true)
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+
+                {
+                    root: '.',
+                    extensions: ['.js', '.ts', '.tsx'],
+                    alias: {
+                        '@components': './src/components',
+                        '@screens': './src/screens',
+                        '@config': './src/config',
+                        '@utils': './src/utils',
+                        '@assets': './src/assets',
+                        '@hooks': './src/hooks',
+                        '@services': './src/services',
+                        '@global': './src/global',
+                        '@routes': './src/routes',
+                        '@context': './src/context',
+                    },
+                },
+            ],
+            'react-native-reanimated/plugin',
+            [
+                'module:react-native-dotenv',
+                {
+                    moduleName: '@env',
+                    allowUndefined: false,
+                },
+            ],
+        ],
+    }
+}
